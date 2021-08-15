73563d4bd9d5a653f8e25c7002065b56569489b3
Teja Guruvelli <guruvelli2012@gmail.com>
2021-08-14 22:11:26 -0400

    feat: new movie app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     results: [],
     selected: {}
   });
-  const apiurl = " http://www.omdbapi.com/?apikey=dd708d5a";
+  const apiurl = "http://www.omdbapi.com/?apikey=dd708d5a";
 
   const search = (e) => {
     if (e.key === "Enter") {
